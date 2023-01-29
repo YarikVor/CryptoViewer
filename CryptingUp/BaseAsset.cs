6eb7d647e036af29003d802462b56cb413b90703
Yarik Vorobyov <official.vorobyov.yaroslav@gmail.com>
2023-01-29 12:59:54 +0200

    Created basic class 'BaseAsset' and methods

---
+++ CryptingUp/BaseAsset.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CryptingUp {
+
+  public class BaseAsset {
+    public const string JSON_PROPERTY_NAME = "assets";
+
+    public string asset_id { get; set; }
+    public string name { get; set; }
+
+    public Asset GetAsset() {
+      if (this is Asset a)
+        return a;
+      return Asset.GetById(asset_id);
+    }
+
+    public static BaseAsset GetById(string asset_id) {
+      return GetAllAsArray().FirstOrDefault(e => e.asset_id == asset_id);
+    }
+
+
+
+    public static BaseAsset[] GetAllAsArray() {
+      string res = CryptingUpMethods.SendGetRequest($"assetsoverview");
+
+      var jObject = JObject.Parse(res);
+      var entities = jObject[JSON_PROPERTY_NAME].ToObject<BaseAsset[]>();
+
+      return entities;
+    }
+  }
+}
