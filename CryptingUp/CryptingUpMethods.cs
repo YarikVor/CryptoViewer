50d9f88aa7a3ab0339228348699f9de834eabb3d
Yarik Vorobyov <official.vorobyov.yaroslav@gmail.com>
2023-01-29 13:02:24 +0200

    Created static class 'CryptingUpMethods', what has constants and method 'SendGetRequest'

---
+++ CryptingUp/CryptingUpMethods.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http;
+
+namespace CryptingUp {
+
+  public static class CryptingUpMethods {
+    public const string DOMAIN = "https://cryptingup.com/api/";
+    public const string PROPERTY_ASSETS = "assets";
+    public const string JSON_NEXT_PROPERTY = "next";
+
+    public static string SendGetRequest(string path) {
+      var client = new HttpClient();
+
+      var result = client.GetAsync(DOMAIN + path).Result;
+      result.EnsureSuccessStatusCode();
+
+      return result.Content.ReadAsStringAsync().Result;
+    }
+  }
+}
