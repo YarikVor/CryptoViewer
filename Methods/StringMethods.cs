0350bf3a459fc94d9b5a06844ccbbd7f19ac78bd
Yarik Vorobyov <official.vorobyov.yaroslav@gmail.com>
2023-02-02 20:01:23 +0200

    Added 'SubstringSize'

---
+++ Methods/StringMethods.cs
@@ -0,0 +1,27 @@
+﻿namespace Methods {
+
+  public static class StringMethods {
+
+    public static int SubstringSize(this string text, string sub) {
+      text = text.ToLower();
+      sub = sub.ToLower();
+      if (sub.Length > text.Length) return 0;
+      int res = 0;
+      int preRes = 0;
+      for (int i = 0, len = text.Length, subLen = sub.Length; i < len - res; i++) {
+        int j = 0;
+        for (; j < subLen && i + j < len; j++) {
+          if (text[i + j] != sub[j]) {
+            break;
+          }
+          preRes++;
+        }
+        if (preRes > res) {
+          res = preRes;
+          preRes = 0;
+        }
+      }
+      return res;
+    }
+  }
+}
