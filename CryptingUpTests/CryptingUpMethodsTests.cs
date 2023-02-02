feeed1ccad12a332ab0c6dca04c4a9ed60a5c38c
Yarik Vorobyov <official.vorobyov.yaroslav@gmail.com>
2023-02-02 20:20:00 +0200

    Added tests for CryptingUpMethods

--- CryptingUpTests/CryptingUpMethodsTests.cs
+++ CryptingUpTests/CryptingUpMethodsTests.cs
@@ -1,19 +1,14 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CryptingUp.Tests {
+
   [TestClass]
   public class CryptingUpMethodsTests {
+
     [TestMethod]
     public void SendGetRequest_IsJsonResult() {
       string res = CryptingUpMethods.SendGetRequest("markets?size=1");
       Assert.IsTrue(res.StartsWith("{\""), "Text must start with '{\"', but started with '" + res.Substring(0, 25) + "'");
     }
-
-
   }
 }
