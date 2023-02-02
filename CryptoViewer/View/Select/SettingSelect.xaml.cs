23073b60081ef7585b06251a5d162934e7ebc37f
Yarik Vorobyov <official.vorobyov.yaroslav@gmail.com>
2023-02-02 20:05:09 +0200

    Added select: Home, Star, Calc and Setting

---
+++ CryptoViewer/View/Select/SettingSelect.xaml.cs
@@ -0,0 +1,43 @@
+﻿using CryptoViewer.Local;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace CryptoViewer {
+
+  public sealed partial class SettingSelect : UserControl {
+
+    public SettingSelect() {
+      this.InitializeComponent();
+      selectLang.SelectedIndex = LocalData.IndexSelectLang;
+      selectTheme.SelectedIndex = LocalData.IndexSelectTheme;
+      selectLang.SelectionChanged += selectLang_SelectionChanged;
+      selectTheme.SelectionChanged += selectTheme_SelectionChanged;
+    }
+
+    private void selectTheme_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+      if (!( sender is ComboBox comboBox )) return;
+
+      ApplicationTheme res;
+      switch (comboBox.SelectedIndex) {
+        case 0: {
+          res = ApplicationTheme.Light;
+          break;
+        }
+        case 1: {
+          res = ApplicationTheme.Dark;
+          break;
+        }
+        default: return;
+      }
+
+      LocalData.Theme = res;
+    }
+
+    private void selectLang_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+      if (!( sender is ComboBox comboBox )) return;
+      LocalData.IndexSelectLang = comboBox.SelectedIndex;
+    }
+  }
+}
---
+++ CryptoViewer/View/Select/StarSelect.xaml.cs
@@ -0,0 +1,85 @@
+﻿using CryptingUp;
+using CryptoViewer.Local;
+using System.ComponentModel;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace CryptoViewer {
+
+  public sealed partial class StarSelect : UserControl, INotifyPropertyChanged {
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public Visibility OverrideVisibility {
+      get => this.Visibility;
+      set {
+        if (this.Visibility != value) {
+          this.Visibility = value;
+          OnPropertyChanged(nameof(OverrideVisibility));
+        }
+      }
+    }
+
+    public StarSelect() {
+      this.InitializeComponent();
+      PropertyChanged += StarSelect_PropertyChanged;
+    }
+
+    public static readonly DependencyProperty OverrideVisibilityProperty = DependencyProperty.Register("OverrideVisibility", typeof(Visibility), typeof(StarSelect), new PropertyMetadata(Visibility.Visible));
+
+    public void OnPropertyChanged(string propertyName) {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    private void StarSelect_PropertyChanged(object sender, PropertyChangedEventArgs e) {
+      if (e.PropertyName != nameof(OverrideVisibility)) {
+        return;
+      }
+      if (this.Visibility == Visibility.Visible) {
+        Update();
+      }
+    }
+
+    public void Update() {
+      scrollBody.Children.Clear();
+
+      var assetsId = LocalHashListAsset.Current;
+
+      if (assetsId.Count > 0) {
+        var assets = Asset.GetAllAsArray();
+        foreach (var asset in assets.Where(e => assetsId.Any(k => k == e.asset_id))) {
+          var assetControl = new AssetControl();
+          assetControl.TappedTitle += btnSelect_Click;
+          assetControl.OnClickSymbolIcon += SymbolIcon_Tapped;
+          assetControl.Title = asset.asset_id;
+          assetControl.Subtitle = asset.name;
+          assetControl.symbolIcon.Symbol = Symbol.SolidStar;
+          scrollBody.Children.Add(assetControl);
+        }
+      } else {
+        var textBlock = new TextBlock();
+        textBlock.Text = "Натисніть на зірочку, щоб додати сюди у список";
+        scrollBody.Children.Add(textBlock);
+      }
+    }
+
+    private void SymbolIcon_Tapped(object sender, TappedRoutedEventArgs e) {
+      if (!( sender is AssetControl assetControl )) return;
+      if (!( assetControl.Title is string assetId )) return;
+
+      if (assetControl.symbolIcon.Symbol == Symbol.SolidStar) {
+        LocalHashListAsset.Remove(assetId);
+        scrollBody.Children.Remove(assetControl);
+      }
+    }
+
+    private void btnSelect_Click(object sender, RoutedEventArgs e) {
+      var frame = Window.Current.Content as Frame;
+      frame.Navigate(typeof(AssetViewer), ( sender as AssetControl ).Title);
+    }
+  }
+}
